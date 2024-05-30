bf2948aa34dadadee4734756686fc871bfdff636
AdityaVS <adityacalvin@gmail.com>
2024-05-30 19:54:44 +1000

    Update Selenium WebDriver test script

--- tests/selenium.test.js
+++ tests/selenium.test.js
@@ -1,15 +1,16 @@
-
 const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 
-const service = new chrome.ServiceBuilder(path.join(__dirname, '../node_modules/.bin/chromedriver')).build();
-chrome.setDefaultService(service);
-
 (async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
+    let service = new chrome.ServiceBuilder(path.join(__dirname, '../node_modules/.bin/chromedriver')).build();
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeService(service)
+        .build();
+    
     try {
-        await driver.get('http://localhost:3000'); // Replace with the actual URL where your app is running
+        await driver.get('http://localhost:8080'); 
         await driver.wait(until.titleIs('WakeCup'), 1000);
         let title = await driver.getTitle();
         console.log(title);
